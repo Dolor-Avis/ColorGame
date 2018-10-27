a12597dcd3dc7c1ee5820a7bfcf8668b5fa46579
Jaszczab <23362384+jaszczab@users.noreply.github.com>
2018-10-27 16:44:02 +0200

    Optimised generateRandColor function

--- colorGame.js
+++ colorGame.js
@@ -51,6 +51,5 @@ function generateRandColor() {
     var r = Math.floor(Math.random() * 256);
     var g = Math.floor(Math.random() * 256);
     var b = Math.floor(Math.random() * 256);
-    var bgColor = "rgb(" + r + ", " + g + ", " + b + ")";
-    return bgColor;
+    return "rgb(" + r + ", " + g + ", " + b + ")";
 }
