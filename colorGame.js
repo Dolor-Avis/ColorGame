8c47b6b6646ccb4a0ea816dc7a7de59aad9e105c
Jaszczab <23362384+jaszczab@users.noreply.github.com>
2018-10-27 16:43:07 +0200

    Added Basic Functionality such as random colors and random choose :)

--- colorGame.js
+++ colorGame.js
@@ -1,29 +1,56 @@
 var squares = document.querySelectorAll(".square");
 var colors = [
-    "rgb(255, 0, 0)",
+/*    "rgb(255, 0, 0)",
     "rgb(0, 255, 0)",
     "rgb(255, 255, 0)",
     "rgb(0, 255, 255)",
     "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)"
+    "rgb(255, 0, 255)"*/
+generateRandColor(),
+generateRandColor(),
+generateRandColor(),
+generateRandColor(),
+generateRandColor(),
+generateRandColor()
 ];
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
-function changeColor() {
-    for (var i = 0;i<squares.length;i++) {
-        //add initial colors to squares
-        squares[i].style.backgroundColor = colors[i];
+var messageDisplay = document.querySelector("#message");
 
-        //add click listeners to squares
-        squares[i].addEventListener("click",function () {
-            var clickedColor = (this.style.backgroundColor);
-            if (clickedColor === pickedColor){
-                alert("Correct");
-            } else {
-                alert("Wrong!!")
-            }
-        });
+
+function changeColor(color) {
+    for (var i = 0; i<squares.length;i++) {
+        squares[i].style.backgroundColor = color;
     }
 }
-changeColor();
+
+for (var i = 0; i < squares.length; i++) {
+    //add initial colors to squares
+    squares[i].style.backgroundColor = colors[i];
+    //add click listeners to squares
+    squares[i].addEventListener("click", function () {
+        var clickedColor = (this.style.backgroundColor);
+        if (clickedColor === pickedColor) {
+            changeColor(clickedColor);
+            messageDisplay.textContent = "Correct";
+        } else {
+            this.style.backgroundColor = "#232323";
+            messageDisplay.textContent = "Try Again";
+        }
+    });
+}
 colorDisplay.textContent = pickedColor;
+
+function pickColor() {
+   var random =  Math.floor(Math.random()*colors.length);
+    return colors[random];
+}
+
+
+function generateRandColor() {
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+    var bgColor = "rgb(" + r + ", " + g + ", " + b + ")";
+    return bgColor;
+}
